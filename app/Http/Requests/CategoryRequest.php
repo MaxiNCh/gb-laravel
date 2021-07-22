bdd7f8dc1faa40224ae0d15d7e4f1f0881f9284c
Maxim Zaboronkin <mvzaboronkin@gmail.com>
2021-07-22 12:34:26 +0300

    Changed CategoryRequest

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -24,7 +24,7 @@ class CategoryRequest extends FormRequest
   public function rules()
   {
     return [
-      'title' => 'required|string',
+      'title' => 'required|unique:categories|string',
       'description' => 'nullable|string',
       'color' => ['required', 'regex:/^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/']
     ];
